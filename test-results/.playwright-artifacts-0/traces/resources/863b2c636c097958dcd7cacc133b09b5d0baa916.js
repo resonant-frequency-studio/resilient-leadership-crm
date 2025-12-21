f6cd63ed9bd3fcea0753b7c4f1eb4cc44b447ce2
Domonic Adams <domonicbritton@gmail.com>
2025-12-20 17:25:04 -0800

    Remove test-results from git tracking and add to .gitignore
    - Added /test-results/ and /playwright-report/ to .gitignore to prevent API keys and sensitive data from being committed
    - Removed test-results directory from git tracking (files may contain API keys in trace data)

--- test-results/.playwright-artifacts-0/traces/resources/863b2c636c097958dcd7cacc133b09b5d0baa916.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_e8d4d024._.js",
-  "static/chunks/node_modules_@tanstack_44296181._.js"
-],
-    source: "dynamic"
-});
